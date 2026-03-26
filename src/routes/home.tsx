10a53eca0630bf3ce511cdf1bfea5863728fc455
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-03-26 22:45:54 +0530

    (v0.3-alpha) Languages Page!
    Added a sample card to store all the languages... ALSO IM BACK AFTER 2 MONTHS. ITs been like soo long. Welcome fellas. Ill complete this proj soon dw

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,7 +3,7 @@ import "../styles/routes/home.css"
 function Home() {
     return (
         <div className="home">
-            <h1>Learn to Code for Free</h1>
+            <h1>Learn to Code for Free.</h1>
         </div>
     )
 }
--- src/routes/languages.tsx
+++ src/routes/languages.tsx
@@ -1,9 +1,51 @@
-import "../styles/routes/home.css"
+import "../styles/routes/languages.css"
 
 function Languages() {
     return (
         <div className="languages">
             <h1>LANGUAGES</h1>
+            <p>Find a range of programming languages to learn.</p>
+
+            <div className={"cards"}>
+                <div className={"card"}>
+                    <div className={"card-cont"}>
+                        <div className={"header"}>
+                            <img src={"https://docs.python.org/3/_static/py.svg"} alt={"python"}/>
+                            <h2>Python</h2>
+                        </div>
+                        <p className={"desc"}>a small description about the language lol. Idk what to write here but i need to write something to make more text.</p>
+                        <div className={"buttons"}>
+                            <a href={"#"}>
+                                <div className={"button"}>Docs </div>
+                            </a>
+                            <a href={"#"}>
+                                <div className={"button"}>Tutorial</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+                <div className={"card"}>
+
+                </div>
+            </div>
         </div>
     )
 }
